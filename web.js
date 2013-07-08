1e5636c545332a3399a8b5d351935749e91d9f05
Oded Priva <startupengineering2@gmail.com>
2013-07-08 09:46:15 +0000

    this is a first try

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var sys = require('sys'),
+    http = require('http'),
+    fs = require('fs'),
+    index;
+ 
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
 });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+ 
+http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.close();
+}).listen(8000);
